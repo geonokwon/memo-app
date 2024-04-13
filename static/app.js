7a62792956bc4695c0f76930af2ae86e9e32404a
geonokown <furitstore1@naver.com>
2024-04-13 16:50:43 +0900

    [memo-app] 간단한 메모앱
    간단하게 CRUD 만들어보기!

---
+++ static/app.js
@@ -0,0 +1,76 @@
+async function editMemo(event) {
+    const id = event.target.dataset.id;
+    const editInput = prompt("수정할 값을 입력하세요");
+    const res = await fetch(`/memos/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id,
+            content: editInput,
+        }),
+    });
+    readMemo();
+}
+
+async function deleteMemo(event) {
+    const id = event.target.dataset.id;
+    const res = await fetch(`/memos/${id}`, {
+        method: "DELETE",
+    });
+    readMemo();
+}
+
+function displayMemo(memo) {
+    const ul = document.querySelector("#memo-ul");
+    const li = document.createElement("li");
+    li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "수정하기";
+    editBtn.addEventListener("click", editMemo);
+    editBtn.dataset.id = memo.id;
+
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "삭제";
+    delBtn.addEventListener("click", deleteMemo);
+    delBtn.dataset.id = memo.id;
+
+    li.appendChild(editBtn);
+    li.appendChild(delBtn);
+    ul.appendChild(li);
+}
+
+async function readMemo() {
+    const res = await fetch("/memos"); //get 요청
+    const jsonRes = await res.json();
+    const ul = document.querySelector("#memo-ul");
+    ul.innerHTML = "";
+    //jsonRes = [{id:123,content:'qqqq'}]
+    jsonRes.forEach(displayMemo);
+}
+
+async function createMemo(value) {
+    const res = await fetch("/memos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id: new Date().getTime(),
+            content: value,
+        }),
+    }); //get요청x post 요청 o
+    readMemo();
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); //submit 의 새로고침을 막는 방법
+    const input = document.querySelector("#memo-input");
+    createMemo(input.value);
+    input.value = "";
+}
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", handleSubmit);
+readMemo();
